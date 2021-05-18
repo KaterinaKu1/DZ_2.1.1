035bcc6adc3f089db3e3de4c885dcf5533b058fa
KaterinaKu1 <katya@sptour.by>
2021-05-18 14:02:42 +0300

    Add files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double ticketPrice = 5893.87;
+        int milecost = 20;
+
+        int bonus = (int) ticketPrice / milecost;
+        System.out.println("Итоговый бонус " + bonus);
+
+    }
+}
